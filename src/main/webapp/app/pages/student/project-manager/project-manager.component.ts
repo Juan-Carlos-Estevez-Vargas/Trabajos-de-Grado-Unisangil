2f352c4e00c15a5e3e3bbe3c7597f022500a9886
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-05-16 16:03:29 -0500

    creación diseño HTML página para seleccionar directores de proyecto junto con su controlador que trae la información en forma de listado de los Tutores

--- src/main/webapp/app/pages/student/project-manager/project-manager.component.ts
+++ src/main/webapp/app/pages/student/project-manager/project-manager.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorService } from 'app/entities/tutor/service/tutor.service';
+import { ITutor } from 'app/entities/tutor/tutor.model';
 
 @Component({
   selector: 'jhi-project-manager',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-manager.component.scss'],
 })
 export class ProjectManagerComponent implements OnInit {
-  constructor() {}
+  managers: ITutor[] = [];
 
-  ngOnInit(): void {}
+  constructor(private tutorService: TutorService) {}
+
+  ngOnInit(): void {
+    this.getTutors();
+  }
+
+  getTutors(): void {
+    this.tutorService.query().subscribe(response => {
+      this.managers = response.body ?? [];
+    });
+  }
 }
